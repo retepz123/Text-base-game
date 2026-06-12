6ad97d5fd605bceeb0132c6e81dc197258938d93
Peter <pedrojuana08@gmail.com>
2026-06-12 20:57:59 +0800

    some introduction

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print ('Welcome to Life Simulator')
+
+startGame  = input('Want to play the game? Yes or No')
+
+if startGame == 'Yes':
+  print('Enjoy the Game')
+  
+elif startGame == 'No':
+  print ('Play it when you have the guts')
+  exit()
+else:
+  print ('Invalid Choice')
+  exit()
+
+
+
+name = input('Write your name here')
+
+print (f'Hello, {name}!')
