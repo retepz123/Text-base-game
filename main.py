5aaca4a25bf9e62fe944ff34ed60e5ba92e72601
Peter <pedrojuana08@gmail.com>
2026-06-16 21:00:38 +0800

    feat: change the approach

--- main.py
+++ main.py
@@ -34,5 +34,25 @@ print(f"Hello, {name},")
 print(f"You are in the {current_place}")
 print(f"Money: {money}\n\nFrom here you can choice your next move. you cann choose 'Mall', 'Univeristy' or to have a part-time job" )
 
+directions = places[current_place]
 
+while True:
+        if directions:
+            print("You can go:")
+        for direction in directions:
+            print(f"- {direction} → {directions[direction]}")
+        else:
+         print("No available moves from here.")
 
+        move = input("\nWhere do you want to go? (or type quit): ").title()
+
+        # quit check first
+        if move == "Quit":
+            break
+
+        # check if move is valid
+        if move in directions:
+            current_place = directions[move]
+        else:
+            print("Invalid move!")
+            input("Press Enter...")
